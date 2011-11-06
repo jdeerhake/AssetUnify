37aed0ab9c25e6cf7167d1a8e2ba31d0de9b3b95
John Deerhake <jdeerhake@gmail.com>
2011-11-05 23:58:22 -0700

    Updates, added readme

--- README.md
+++ README.md
@@ -1,1 +1,102 @@
-In progress... 
+AssetUnify - asset packaging for PHP.
+====================================
+
+Configration
+------------
+
+Create a file at DOCUMENT_ROOT/config/assets.json.  Define packages using JSON like so:
+
+    {
+      "scripts" : {
+        "packages" : {
+          "cats" : ["cheetah", "leopard"],
+          "dogs" : ["greyhound", "shephard"]
+        }
+      },
+      "stylesheets" : {
+        "packages" : {
+          "cats" : ["cheetah", "leopard"],
+          "dogs" : ["greyhound", "shephard"]
+        }
+      }
+    }
+
+Include a "directory" key under either scripts or stylesheet will set the directory where they are located (relative to DOCUMENT_ROOT, defaults are javascripts and stylesheets, respectively):
+
+    {
+      "scripts" : {
+        "directory" : "/js",
+        ...
+      },
+      "stylesheets" : {
+        "directory" : "/css"
+      }
+    }
+
+To run minification or any sort of modifcation on the assets, provide a "minify" key.  Its value should be the function name that the files contents should be called with. For example:
+
+    {
+      "scripts" : {
+        "minify" : "run_jsmin"
+        ...
+      },
+      ...
+    }
+
+Would cause the contents of each file to run through the global function "run_jsmin" like this:
+
+    $file_contents = run_jsmin($file_contents);
+
+(Soon minification will be built into AssetUnify)
+
+
+Usage
+-----
+
+Example page:
+
+    <?php
+    require_once($_SERVER['document_root'] . "lib/asset_unify.php");
+    AssetUnify\Packager::$env = "development"; //Currently required to function. Instructs it to just include each file as a separate tag
+    ?>
+    <!doctype html>
+    <html>
+      <head>
+        <?= AssetUnify\include_stylesheets("dogs"); //Can pass just string name of a package ?>
+      </head>
+      <body>
+        ...
+        <?= AssetUnify\include_scripts(array("dogs", "cats")); //Or an array naming multiple packages ?>
+      </body>
+    </html>
+
+
+
+Example file structure
+----------------------
+
+File layout used for the above examples.
+
+
+  DOCUMENT_ROOT
+  │
+  ├── lib
+  │   └── asset_unify.php
+  │
+  ├── config
+  │   └── assets.json
+  │
+  ├── stylesheets
+  │   ├── leopard.css
+  │   ├── cheetah.css
+  │   ├── greyhound.css
+  │   └── shephard.css
+  │
+  ├── javascripts
+  │   ├── leopard.js
+  │   ├── cheetah.js
+  │   ├── greyhound.js
+  │   └── shephard.js
+  │
+  └── index.php
+
--- asset_unify.php
+++ asset_unify.php
@@ -2,6 +2,53 @@
 namespace AssetUnify;
 
 /*******************
+ *  Packager
+ *******************/
+class Packager {
+  public static $root;
+  public static $defaults;
+  public static $config;
+  public static $script_packages = array();
+  public static $stylesheet_packages = array();
+  public static $web_root_dir;
+  public static $env = "production";
+}
+
+Packager::$defaults = array(
+  "root" => $_SERVER['DOCUMENT_ROOT'],
+  "config_file" => array("config", "assets.json"),
+  "web_root" => array(""),
+  "scripts" => array(
+    "directory" => array("javascripts"),
+    "output" => array("assets"),
+    "minify" => false
+  ),
+  "stylesheets" => array(
+    "directory" => array("stylesheets"),
+    "output" => array("assets"),
+    "minify" => false
+  ),
+);
+
+
+
+/*******************
+ *  Read config
+ *******************/
+call_user_func(function() {
+  $config = new File(Packager::$defaults['root'], Packager::$defaults['config_file']);
+
+  $config = json_decode($config->contents(), true);
+  if(is_null($config)) trigger_error("Error parsing config file.", E_USER_ERROR);
+  $config = array_merge_recursive(Packager::$defaults, $config);
+  Packager::$config = $config;
+
+  Packager::$web_root_dir = File::join_paths(Packager::$config['root'], Packager::$config['web_root']);
+});
+
+
+
+/*******************
  *  File
  *******************/
 class File {
@@ -9,16 +56,42 @@ class File {
     return join(DIRECTORY_SEPARATOR, $arr);
   }
 
-  function __construct($path) {
-    if(is_array($path)) {
-      $this->path = File::dir_from_array($path);
+  public static function join_paths($begin, $end) {
+    return File::dir_from_array(array_merge((array)$begin, (array)$end));
+  }
+
+  public static function ensure_leading_slash($dir) {
+    if(substr($dir, 0, 1) != DIRECTORY_SEPARATOR) {
+      return DIRECTORY_SEPARATOR . $dir;
     } else {
-      $this->path = $path;
+      return $dir;
     }
   }
 
+  function __construct($base, $relative_path) {
+    if(is_array($relative_path)) {
+      $this->relative_path = File::dir_from_array($relative_path);
+    } else {
+      $this->relative_path = $relative_path;
+    }
+
+    if(is_array($base)) {
+      $this->base = File::dir_from_array($base);
+    } else {
+      $this->base = $base;
+    }
+  }
+
+  function path() {
+    return File::join_paths($this->base, $this->relative_path);
+  }
+
   function contents() {
-    return file_get_contents($this->path);
+    return file_get_contents($this->path());
+  }
+
+  function url() {
+    return File::ensure_leading_slash($this->relative_path);
   }
 }
 
@@ -27,10 +100,15 @@ class File {
  *  ScriptFile
  *******************/
 class ScriptFile extends File {
-  function __construct($path) {
-    parent::__construct($path);
-    if(substr($this->path, -3, 3) != ".js") $this->path = $this->path . ".js";
+  function __construct($base, $path) {
+    parent::__construct($base, $path);
+    if(substr($this->relative_path, -3, 3) != ".js") $this->relative_path = $this->relative_path . ".js";
   }
+
+  function tag() {
+    return "<script type='text/javascript' src='" . $this->url() . "'></script>";
+  }
+
 }
 
 
@@ -38,9 +116,13 @@ class ScriptFile extends File {
  *  StylesheetFile
  *******************/
 class StylesheetFile extends File {
-  function __construct($path) {
-    parent::__construct($path);
-    if(substr($this->path, -4, 4) != ".css") $this->path = $this->path . ".css";
+  function __construct($base, $path) {
+    parent::__construct($base, $path);
+    if(substr($this->relative_path, -4, 4) != ".css") $this->relative_path = $this->relative_path . ".css";
+  }
+
+  function tag() {
+    return "<link rel='stylesheet' type='text/css' media='all' href='" . $this->url() . "' />";
   }
 }
 
@@ -67,6 +149,18 @@ class Package {
     }
 
     return $contents;
+  }
+
+  function tag() {
+    if(Packager::$env == "development") {
+      $tags = array();
+
+      foreach($this->files as $file) {
+        array_push($tags, $file->tag());
+      }
+
+      return join("\n", $tags);
+    }
   }
 
   function push($file) {
@@ -98,6 +192,7 @@ class ScriptPackage extends Package {
   function inline_contents() {
     return "<script type='text/javascript'>\n" . $this->contents() . "\n</script>";
   }
+
 }
 
 /************************
@@ -119,60 +214,21 @@ class StylesheetPackage extends Package {
   }
 }
 
-/*******************
- *  Packager
- *******************/
-class Packager {
-  public static $root;
-  public static $defaults;
-  public static $config;
-  public static $script_packages = array();
-  public static $stylesheet_packages = array();
-
-}
-
-Packager::$root = $_SERVER['DOCUMENT_ROOT'];
-Packager::$defaults = array(
-  "config_file" => array(Packager::$root, "config", "assets.json"),
-  "scripts" => array(
-    "directory" => array(Packager::$root, "public", "javascripts"),
-    "output" => array(Packager::$root, "public", "assets"),
-    "minify" => false
-  ),
-  "stylesheets" => array(
-    "directory" => array(Packager::$root, "public", "stylesheets"),
-    "output" => array(Packager::$root, "public", "assets"),
-    "minify" => false
-  ),
-);
-
-
-
-/*******************
- *  Read config
- *******************/
-call_user_func(function() {
-  $config = new File(Packager::$defaults['config_file']);
-  $config = json_decode($config->contents(), true);
-  if(is_null($config)) trigger_error("Error parsing config file.", E_USER_ERROR);
-  if(isset($config['scripts']['dir'])) $config['scripts']['dir'] = array_merge((array)Packager::$root, (array)$config['scripts']['dir']);
-  if(isset($config['stylesheets']['dir'])) $config['stylesheets']['dir'] = array_merge((array)Packager::$root, (array)$config['stylesheets']['dir']);
-  $config = array_merge(Packager::$defaults, $config);
-  Packager::$config = $config;
-});
-
-
 /**************************
  *  Init script packages
  **************************/
 call_user_func(function() {
   $script_packages = array();
+
   foreach(Packager::$config["scripts"]["packages"] as $name => $paths) {
     $current_package = $script_packages[$name] = new ScriptPackage(null);
+
     foreach($paths as $path) {
-      $current_package->push(new ScriptFile(array_merge(Packager::$config["scripts"]["dir"],(array)$path)));
+      $file = new ScriptFile(Packager::$web_root_dir, File::join_paths(Packager::$config["scripts"]["directory"], $path));
+      $current_package->push($file);
     }
   }
+
   Packager::$script_packages = $script_packages;
 });
 
@@ -181,15 +237,22 @@ call_user_func(function() {
  *****************************/
 call_user_func(function() {
   $stylesheet_packages = array();
+
   foreach(Packager::$config["stylesheets"]["packages"] as $name => $paths) {
-    $current_package = $stylesheet_packages[$name] = new StylesheetPackage(null);
+    $current_package = $stylesheet_packages[$name] = new stylesheetPackage(null);
+
     foreach($paths as $path) {
-      $current_package->push(new StylesheetFile(array_merge(Packager::$config["stylesheets"]["dir"],(array)$path)));
+      $file = new stylesheetFile(Packager::$web_root_dir, File::join_paths(Packager::$config["stylesheets"]["directory"], $path));
+      $current_package->push($file);
     }
   }
+
   Packager::$stylesheet_packages = $stylesheet_packages;
 });
 
+/*****************************
+ *  Initialize
+ *****************************/
 
 
 /*****************************
@@ -201,7 +264,7 @@ function is_javascript_package($name) {
 }
 
 
-function include_scripts($package_names, $options) {
+function include_scripts($package_names, $options = array("type" => "external")) {
   $packages = array();
   foreach((array)$package_names as $package_name) {
      if(is_javascript_package($package_name)) array_push($packages, Packager::$script_packages[$package_name]);
@@ -209,7 +272,11 @@ function include_scripts($package_names, $options) {
 
 
   foreach($packages as $package) {
-    echo $package->inline_contents() . "\n";
+    if($options["type"] == "inline") {
+      echo $package->inline_contents() . "\n";
+    } else {
+      echo $package->tag() . "\n";
+    }
   }
 }
 
@@ -223,15 +290,18 @@ function is_stylsheet_package($name) {
 }
 
 
-function include_stylesheets($package_names, $options) {
+function include_stylesheets($package_names, $options = array("type" => "external")) {
   $packages = array();
   foreach((array)$package_names as $package_name) {
      if(is_stylsheet_package($package_name)) array_push($packages, Packager::$stylesheet_packages[$package_name]);
   }
 
-
   foreach($packages as $package) {
-    echo $package->inline_contents() . "\n";
+    if($options["type"] == "inline") {
+      echo $package->inline_contents() . "\n";
+    } else {
+      echo $package->tag() . "\n";
+    }
   }
 }
 
